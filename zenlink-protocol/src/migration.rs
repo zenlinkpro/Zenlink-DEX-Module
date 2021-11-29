f295cd49937c6146353a78a4521d289584be7cc3
byterui <shirui_yj133@163.com>
2021-11-29 18:01:09 +0800

    fix migration weight

--- zenlink-protocol/src/migration.rs
+++ zenlink-protocol/src/migration.rs
@@ -5,5 +5,5 @@ use sp_core::U256;
 pub fn update_k_value_type_to_u256<T: Config>() -> Weight {
     KLast::<T>::translate_values::<u128, _>(|v| Some(U256::from(v)));
 
-    T::DbWeight::get().writes(4) + T::DbWeight::get().writes(4)
+    T::DbWeight::get().reads(4) + T::DbWeight::get().writes(4)
 }
