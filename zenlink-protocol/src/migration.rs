b10ebd79804e1bd045e798bc361e3382fedf9d78
byterui <shirui_yj133@163.com>
2021-11-30 09:52:30 +0800

    update k value migration

--- zenlink-protocol/src/migration.rs
+++ zenlink-protocol/src/migration.rs
@@ -3,7 +3,13 @@ use frame_support::traits::Get;
 use sp_core::U256;
 
 pub fn update_k_value_type_to_u256<T: Config>() -> Weight {
-    KLast::<T>::translate_values::<u128, _>(|v| Some(U256::from(v)));
+    KLast::<T>::translate_values::<u128, _>(|v|
+        if v == u128::MAX{
+            Some(U256::zero())
+        }else{
+            Some(U256::from(v))
+        }
+    );
 
     T::DbWeight::get().reads(4) + T::DbWeight::get().writes(4)
 }
