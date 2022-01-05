ec3425f48f0278eb1542c8b57e27b453722ef39f
byterui <shirui_yj133@163.com>
2022-01-05 17:18:26 +0800

    fix bench

--- zenlink-protocol/src/benchmarking.rs
+++ zenlink-protocol/src/benchmarking.rs
@@ -101,7 +101,7 @@ benchmarks! {
 			10*UNIT,
 			10*UNIT,
 			99u128.saturated_into()
-			[ASSET_0, ASSET_1].to_vec(),
+			[ASSET_0].to_vec(),
 			[(ASSET_0, 2 * UNIT), (ASSET_1, 1 * UNIT)].to_vec()
 		));
 
@@ -146,8 +146,8 @@ benchmarks! {
 			10*UNIT,
 			10*UNIT,
 			99u128.saturated_into(),
-			[].to_vec(),
-			[].to_vec()
+			[ASSET_0].to_vec(),
+			[(ASSET_0, 2 * UNIT)].to_vec()
 		));
 
 		assert_ok!(ZenlinkPallet::<T>::bootstrap_contribute(
